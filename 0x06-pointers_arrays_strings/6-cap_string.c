b13580967a71e22dd74be57b85bcb163235d2bf6
Cynthia <cynthiam6331@gmail.com>
2023-03-03 04:28:14 -0800

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  *
- * Return: A pointer to the changed string.
+ * Return: A pointer to be changed string.
  */
 char *cap_string(char *str)
 {
@@ -18,6 +18,7 @@ char *cap_string(char *str)
 		if (str[index - 1] == '' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
+		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
 		    str[index - 1] == ',' ||
 		    str[index - 1] == '.' ||
@@ -29,14 +30,12 @@ char *cap_string(char *str)
 		    str[index - 1] == '{' ||
 		    str[index - 1] == '}' ||
 		    index == 0)
-			str[index] -= 32;
+		       str[index] -= 32;
 
-		index++;
+		      index++;
 	}
 
 	return (str);
 
 }
-
-
 
